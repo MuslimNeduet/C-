71ed9f7190ff03bcdef51d02f1c428da1a207b50
Muslimneduet <muslim.neduet@gmail.com>
2025-01-16 18:05:56 +0500

    Added CHECKING

---
+++ checking.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<cstdio>
+#include<cmath>
+#include<array>
+using namespace std;
+int main(){
+    cout<<"Hello World!";
+return 0;
+}
