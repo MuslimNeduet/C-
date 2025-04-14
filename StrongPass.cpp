95346059e593ee00aa38738cd84344de687068ab
Muslimneduet <muslim.neduet@gmail.com>
2025-04-14 14:53:58 +0500

    Strongpass

---
+++ StrongPass.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string a = "abcdefghijklmnopqrstuvwxyz";
+    string b = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    string c = "1234567890";
+    string d = "!@#$%^&*()";
+
+    
+    string password;
+    cout<<"Enter Your Password: "<<endl;
+    cin>>password;
+    cout<<"Your Password Is: "<<password<<endl;
+
+    int length = password.size();
+    int counter = 0;    
+    if(length<6){
+        counter=counter+2;
+    }
+
+    bool w,x,y,z = 0;
+
+    for(int i = 0; i<length; i++){
+            if(a.find(password[i])!=string::npos){
+                w=1;
+            }
+        }
+
+    for(int i = 0; i<length; i++){
+            if(b.find(password[i])!=string::npos){
+                x=1;
+            }
+        }
+
+
+    for(int i = 0; i<length; i++){
+            if(c.find(password[i])!=string::npos){
+                y=1;
+            }
+        }
+
+    for(int i = 0; i<length; i++){
+            if(d.find(password[i])!=string::npos){
+                z=1;
+            }
+        }
+
+    if(w==0){
+        counter++;
+    }
+    if(x==0){
+        counter++;
+    }
+    if(y==0){
+        counter++;
+    }
+    if(z==0){
+        counter++;
+    }
+    cout<<counter<<endl;
+}
