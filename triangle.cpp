76cd99370f8f943503ee53aa3818885c9fc0aa48
Muslimneduet <muslim.neduet@gmail.com>
2025-04-19 16:29:32 +0500

    staircase

--- triangle.cpp
+++ triangle.cpp
@@ -11,8 +11,11 @@ int main(){
     cout<<"Enter The Number: ";
     cin>>n;
     for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= i; j++){
-            cout<<"* ";
+        for(int j = n; j > i; j--){
+            cout<<" ";
+        }
+        for(int k = 1; k <= i; k++){
+            cout<<"#";
         }
         cout<<endl;
     }
