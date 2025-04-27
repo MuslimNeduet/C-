fe03b9acefd2d3173b119ab113f077d33a92dcd8
Muslimneduet <muslim.neduet@gmail.com>
2025-04-27 23:54:30 +0500

    Summation

---
+++ summation.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter First Number: ";
+    cin>>a;
+    cout<<"Enter Second Number: ";
+    cin>>b;
+    cout<<"The Sum Is: "<<a+b;
+}
